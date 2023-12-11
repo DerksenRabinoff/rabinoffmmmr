1934be9da3fbc611f98ac417e91c713e885398bc
drabinoff <d.rabinoff@gmail.com>
2023-12-10 23:31:32 -0600

    Enhance plotting functions

--- R/mmm_plots.R
+++ R/mmm_plots.R
@@ -11,12 +11,14 @@
 #' @param rate If TRUE, the y axis will be the return on the next dollar spent instead of the return on the current spend level.
 #' @param xy_only If TRUE, return a dataframe of the x-y points for the plot. FALSE by default.
 #' @param inflection_point If TRUE, the plot will be annotated at the inflection point.
-#' @param ... Arguments passed to a theme object for the plot
+#' @param x_scale A scale function applied to the x-axis labels and the x value of the inflection point
+#' @param y_scale A scale function applied to the y-axis labels and the y value of the inflection point
+#' @param ... Arguments passed to the text geom with the inflection point
 #' 
 #' @return If xy_only is false, a ggplot2 object is returned. Otherwise, a dataframe with x-y coordinates is returned.
 #' 
 #' @export
-plot_diminishing_returns <- function(object, channel, rate = FALSE, xy_only = FALSE, inflection_point = TRUE, ...){
+plot_diminishing_returns <- function(object, channel, rate = FALSE, xy_only = FALSE, inflection_point = TRUE, x_scale = scales::dollar_format(), y_scale = scales::dollar_format(), ...){
 
     hyps <- coef(object, complete = TRUE, params = TRUE) %>%
         dplyr::filter(predictors == channel)
@@ -64,15 +66,14 @@ plot_diminishing_returns <- function(object, channel, rate = FALSE, xy_only = FA
     g <- g +
         ggplot2::geom_line() +
         ggplot2::labs(title = paste(names(plotframe), collapse = " ")) +
-        ggplot2::theme(...)
+        ggplot2::scale_x_continuous(labels = x_scale) +
+        ggplot2::scale_y_continuous(labels = y_scale)         
     
     if(inflection_point){
         inflect_x <- plotframe[min(abs(xs - hyps$gammaTrans)),]
         g <- g + ggplot2::annotate(geom = "point", x = inflect_x[1,1], y = inflect_x[1,2]) +
-            ggplot2::annotate(geom = "text",
-                              x = inflect_x[1,1],
-                              y = inflect_x[1,2],
-                              label = paste("(", floor(inflect_x[1,1]), ",", floor(inflect_x[1,2]), ")", collapse=""))
+            ggplot2::geom_text(data = inflect_x,
+                              label = paste("(", x_scale(floor(inflect_x[1,1])), ",", y_scale(floor(inflect_x[1,2])), ")", collapse=""), ...)
         }
     
     return(g)
